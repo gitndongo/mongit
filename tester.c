585cb3efab62d9fdef157587575d4656db1421b7
gitndongo <mouhamedkane54858@gmail.com>
2025-03-20 12:49:26 +0000

    apres restauration de tester.c

---
+++ tester.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("ce progrmme vous permez de faire des calcule comme vous le savez");
+    printf("voici ce que j'ai modifie depuis github");
+ return 0;   
+}
