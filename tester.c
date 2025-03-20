f1c7bb6952e9ca2f3c4e9fd1e0ad3e7c3d0f68c7
gitndongo <mouhamedkane54858@gmail.com>
2025-03-20 12:14:31 +0000

    apres avoir supprimer test.c

--- tester.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("ce progrmme vous permez de faire des calcule comme vous le savez");
-    printf("voici ce que j'ai modifie depuis github");
- return 0;   
-}
